17b2894eb31ba36d35323e1b3ec61d992aa64634
fausto4911 <fausto4911@gmail.com>
2020-04-26 19:15:45 -0400

    sample code to connect to database was create

--- main/main.go
+++ main/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"io"
 	"log"
+	"main/dbconnection"
 	"net/http"
 )
 
 func main() {
 	// Hello world, the web server
-
+	dbconnection.GetPostingDb()
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
